16be35ba7409ad74a2aeb41236df178f523e306d
Leyan <leanne_feng@yahoo.com>
2022-11-20 13:41:03 -0800

    recreate prefabs

--- Assets/Scripts/PusherController.cs
+++ Assets/Scripts/PusherController.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PusherController : MonoBehaviour
 {
+    public float megaDozerMultiplier =2f;
     private Vector3 origin;
     private Rigidbody rigidbody => GetComponent<Rigidbody>();
     // Update is called once per frame
     void Update()
     {
         Vector3 offset = new Vector3(0, 0, Mathf.Sin(Time.time));
-        rigidbody.MovePosition(origin + offset);
+        rigidbody.MovePosition(origin + offset* megaDozerMultiplier);
     }
 
     void Awake()
