f504f25b25c1ed6006da3762ba550f2bcc5cefa9
LC <83504712+bountyhat@users.noreply.github.com>
2022-11-21 13:31:28 +0800

    Add Click and drop coin script
    - Repack prefab, updated in "CoinMachine" prefab
    - Added physics materials
    - Open new scene called "Game"
    - Change texture of coin

---
+++ Assets/Scripts/ClickArea.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickArea : MonoBehaviour
+{
+	public GameObject coinPrefab;
+	public float height;
+
+    private void OnMouseDown()
+	{
+		AddCoinOnUserClicked();
+    }
+    public void AddCoinOnUserClicked()
+	{
+        /*      IsPlayerAllowed to click? (player not allowed to click to frequently)
+                then is playercoin > 0 ?*/
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            Instantiate(coinPrefab,
+                         new Vector3(hit.point.x, height, hit.point.z),
+                         Quaternion.identity);
+
+            //Reduce coin in the inventory
+        }
+    }
+}
